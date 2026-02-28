449a6a841db08e32c4528572d88b8a115d311243
nasirusaliu <nasirusaliu04@gmail.com>
2026-02-28 15:58:01 +0300

    edits

--- Practice/Advanced Programming/Practice Questions/PQW2.py
+++ Practice/Advanced Programming/Practice Questions/PQW2.py
@@ -1,6 +1,16 @@
+#Question1
 with open("numbers.txt","w") as f:
     for num in range(1,101):
         f.write(f"{num}\n")
+
+
+#Question2
 with open("numbers.txt","r") as f:
+    total = 0
     for line in f:
-        print(line.strip())
+        # print(type(line))
+        lines = int(line.strip())
+        total +=lines
+
+with open("numbers.txt","a") as f:
+        f.write(f"\nThe sum of these numbers are:{str(total)}\n")
